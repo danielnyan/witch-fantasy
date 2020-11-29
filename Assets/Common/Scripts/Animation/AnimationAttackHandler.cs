add1e082af9c85f771adbab16a1843f1754ceb0b
danielnyan <E0235108@u.nus.edu>
2020-11-29 10:52:15 +0800

    (wip) attack framework

---
+++ Assets/Common/Scripts/Animation/AnimationAttackHandler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationAttackHandler : MonoBehaviour
+{
+    private Animator animator;
+    private AnimatorOverrideController overrideController;
+
+    private AnimationUpdater currentUpdater;
+    private AnimationHandlerEvent eventListener;
+    [SerializeField]
+    private RuntimeAnimatorController defaultController;
+    public AnimationUpdater test;
+
+    public void UpdateAnimations(AnimationUpdater updater)
+    {
+        // To do: reset everything first
+        eventListener.RemoveAllListeners();
+        currentUpdater = updater;
+        foreach (AnimationData d in updater.animationData)
+        {
+            Transform parent = transform;
+            if (d.parent != "")
+            {
+                parent = transform.Find(d.parent);
+            }
+            GameObject o = Instantiate(d.gameObject);
+            o.transform.parent = parent;
+            o.transform.localPosition = d.position;
+            o.transform.localRotation = Quaternion.Euler(d.rotation);
+            // To do: implement scale feature
+            o.transform.localScale = Vector3.one;
+            o.name = d.objectName;
+        }
+        overrideController = new AnimatorOverrideController();
+        overrideController.runtimeAnimatorController = defaultController;
+        foreach (OverridePair p in updater.overridePair)
+        {
+            overrideController[p.name] = p.clip;
+        }
+        animator.runtimeAnimatorController = overrideController;
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        overrideController = new AnimatorOverrideController(animator.runtimeAnimatorController);
+        eventListener = new AnimationHandlerEvent();
+        UpdateAnimations(test);
+    }
+}
---
+++ Assets/Common/Scripts/Animation/AnimationUpdater.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct AnimationData
+{
+    public GameObject gameObject;
+    public string objectName;
+    public string parent;
+    public Vector3 position;
+    public Vector3 rotation;
+}
+
+[Serializable]
+public struct OverridePair
+{
+    public string name;
+    public AnimationClip clip;
+}
+
+[CreateAssetMenu(fileName ="Animation Updater", menuName ="ScriptableObject/AnimationUpdater")]
+public class AnimationUpdater : ScriptableObject
+{
+    [SerializeField]
+    public AnimationData[] animationData;
+    [SerializeField]
+    public OverridePair[] overridePair;
+}
